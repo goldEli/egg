0e4d290739a149ec1415824b8611092ce7024576
miaoyu <miaoyu@sefonsoft.com>
2020-12-07 16:58:47 +0800

    add router

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,11 +1,15 @@
-'use strict';
+"use strict";
 
-const Controller = require('egg').Controller;
+const Controller = require("egg").Controller;
 
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, egg';
+    ctx.body = "hello, world!";
+  }
+  async demo() {
+    const { ctx } = this;
+    ctx.body = "this is demo";
   }
 }
 
