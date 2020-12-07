d39e64c0e3b070edb31af2920f0580c584e5b99d
miaoyu <miaoyu@sefonsoft.com>
2020-12-07 18:02:41 +0800

    system info

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,10 +1,12 @@
 "use strict";
 
 const Controller = require("egg").Controller;
+const info = require("../utils/info")
 
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
+    console.log(info())
     ctx.body = "hello, world!";
   }
   async demo() {
