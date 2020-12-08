9b628fde2c9bdc5f30880e7719d8e3728ed7f8c2
miaoyu <miaoyu@sefonsoft.com>
2020-12-08 16:41:50 +0800

    store session

--- app.js
+++ app.js
@@ -5,4 +5,18 @@ module.exports = app => {
   app.messenger.on('agentAction', data => {
     console.log(data);
   });
+  const store = {};
+
+  app.sessionStore = {
+    async get(key) {
+      console.log('--store--', store[key]);
+      return store[key];
+    },
+    async set(key, value, maxAge) {
+      store[key] = value;
+    },
+    async destroy(key) {
+      store[key] = null;
+    },
+  };
 };
