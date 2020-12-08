a8dd2f11d4dac79636a62c278b1ba33dce3716b9
miaoyu <miaoyu@sefonsoft.com>
2020-12-08 11:29:16 +0800

    test await in get request

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,11 +1,20 @@
-'use strict'
+'use strict';
 
 const Controller = require('egg').Controller;
 
 class UserController extends Controller {
   async index() {
     const { ctx, app } = this;
-    ctx.body = "user index"
+    ctx.body = 'user index';
+  }
+  async lists() {
+    const {ctx} = this;
+    await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve()
+      }, 5000)
+    })
+    ctx.body = [{id: "1", name: "eli"}]
   }
 }
 
