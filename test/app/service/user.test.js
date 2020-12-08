14af428fab439ca32b1f6fca32aa771a50b1875b
miaoyu <miaoyu@sefonsoft.com>
2020-12-08 15:01:24 +0800

    test only

--- test/app/service/user.test.js
+++ test/app/service/user.test.js
@@ -4,7 +4,7 @@ const {app, assert} = require("egg-mock/bootstrap")
 
 
 describe("service user test", () => {
-  it('test detail', async () => {
+  it.only('test detail', async () => {
     const ctx = app.mockContext()
     const user = await ctx.service.user.detail(10)
     assert(user)
